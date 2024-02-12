d6e9496fdac1ca510c30a05b7cb100954d1e72fb
Débora Bastos <105684598+deborabastos96@users.noreply.github.com>
2024-02-12 09:08:36 -0300

    style: add blur on mobile menu

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -10,28 +10,48 @@ const StyledSidebar = styled.aside`
   height: 100vh;
   min-width: 26rem;
   background-color: var(--color-grey-0);
-  z-index: 4;
   padding: 8rem 2.4rem 3.2rem;
   border-right: 1px solid var(--color-grey-100);
   transition: transform 0.3s ease-in-out;
   transform: ${({ $isMenuOpen }) =>
     $isMenuOpen ? "translateX(0)" : "translateX(-100%)"};
+  z-index: 4;
 
   display: flex;
   flex-direction: column;
   gap: 3.2rem;
+  box-shadow: var(--shadow-lg);
 
   @media only screen and ${device.tabLand} {
     grid-row: 1 / -1;
     position: relative;
     padding: 3.2rem 2.4rem;
     transform: translateX(0);
+    box-shadow: none;
+  }
+`;
+
+const Overlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  min-width: 100vw;
+  min-height: 100vh;
+  background: ${({ $isMenuOpen }) =>
+    $isMenuOpen ? "rgb(111, 111, 111, 0.5)" : "rgb(111, 111, 111, 0)"};
+  z-index: 2;
+
+  @media only screen and ${device.tabLand} {
+    width: 0;
+    height: 0;
+    z-index: -1;
   }
 `;
 
 function Sidebar({ isMenuOpen, setIsMenuOpen }) {
   return (
     <StyledSidebar $isMenuOpen={isMenuOpen}>
+      <Overlay $isMenuOpen={isMenuOpen} />
       <Logo />
       <MainNav setIsMenuOpen={setIsMenuOpen} />
     </StyledSidebar>
